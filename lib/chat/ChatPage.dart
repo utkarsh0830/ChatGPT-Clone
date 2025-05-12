8fcb1701dc2962cb6fbbe8da9d7756232cb82fd0
Utkarsh Agrawal <149224355+utkarsh0830@users.noreply.github.com>
2025-05-12 13:52:58 +0530

    Chat Page finished

--- lib/chat/ChatPage.dart
+++ lib/chat/ChatPage.dart
@@ -20,7 +20,16 @@ class _ChatpageState extends State<Chatpage> {
       body: Container(
         child: Column(
           children: [
-            Expanded(child: ListView()),
+            Expanded(child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                promptContainer("Tell Me Something"),
+                promptContainer("Tell Me Something"),
+                promptContainer("Tell Me Something"),
+                promptContainer("Tell Me Something"),
+              ],
+              )
+            ),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 12),
               decoration: BoxDecoration(
@@ -28,12 +37,12 @@ class _ChatpageState extends State<Chatpage> {
                 borderRadius: BorderRadius.circular(6)
               ),
               margin: EdgeInsets.only(
-                bottom: 40,
+
                 left: 16,
                 right: 16,
                 top: 6
               ),
-              child: Row(
+              child: const Row(
                 children: [
                   Expanded(child: TextField(
                     cursorColor: Colors.white,
@@ -54,9 +63,49 @@ class _ChatpageState extends State<Chatpage> {
                 ],
               ),
             ),
+
+            const SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  Text("ChatGPT March 2025",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline
+                  ),),
+                  const SizedBox(width: 10,),
+                  Text("Free Research Preview")
+                ],
+              ),
+            ),
+            const SizedBox(height: 10,),
           ],
         ),
       ),
     );
   }
+}
+
+Widget promptContainer(String text){
+  return Container(
+    margin: EdgeInsets.only(right: 8),
+    //color: Colors.amber.withOpacity(0.1),
+    height: 120,
+    padding: const EdgeInsets.all(16),
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(
+          width: 200,
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white,width: 0.5),
+              color: Colors.white.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(8)
+          ),
+          child: Text(text),
+        )
+      ],
+    ),
+  );
 }
